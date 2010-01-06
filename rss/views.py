77a1df30d9d29e74d34d9a1ff495c00222b6f777
dvydra <usefulaccount@gmail.com>
2010-01-06 16:04:07 +0000

    parsing for the age works

--- rss/views.py
+++ rss/views.py
@@ -14,19 +14,19 @@ def importFeed(request,url):
     feed = feedparser.parse(url)    
     feed_items = []    
 
-    for item in feed["items"][0:1]:
+    for item in feed["items"]:
         item_content = urllib2.urlopen(item["link"]).read()
         soup = BeautifulSoup.BeautifulSoup(item_content)
-        body = soup.find('div',{"class":"articleBody"}, text=True)
+        body = soup.find('div',{"class":"articleBody"})
         feed_item = PyRSS2Gen.RSSItem(title=item["title"], 
                                         link=item["link"], 
                                         guid=item["guid"], 
                                         pubDate=item["updated"],
-                                        description=body)
+                                        description=unicode(body))
         feed_items.append(feed_item)
     
     rss = PyRSS2Gen.RSS2(title=feed["channel"]["title"], link=feed["channel"]["link"], description=feed["channel"]["description"], lastBuildDate = datetime.datetime.now(),
                         items= feed_items)
                                 
-    return HttpResponse(rss.to_string)
+    return HttpResponse(rss.to_xml())
     
